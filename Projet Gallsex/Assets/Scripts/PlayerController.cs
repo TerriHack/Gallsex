ffe530995a52b9da37015940a3f1cc506a96c7c8
Mskutnik <m.skutnik@rubika-EDU.com>
2022-03-04 10:16:48 +0100

    Hookshot
    Ajouté un line renderer, créé 3 fonctions, une pour utiliser le line renderer, une pour le raycast du grappin et une pour la traction du grappin

--- Projet Gallsex/Assets/Scripts/PlayerController.cs
+++ Projet Gallsex/Assets/Scripts/PlayerController.cs
@@ -21,6 +21,13 @@ public class PlayerController : MonoBehaviour
     private InputAction _jumpAction;
     private InputAction _horizontalMove;
 
+    [Header("Hookshot related")]
+    private float linePosX; // Variables pour le grappin - Martin
+    private float linePosY;
+    private bool cangrapple = false;
+    public LineRenderer line;
+    [SerializeField] private LayerMask groundlayer;
+
     #region Private var
     private Vector2 _playerPos;
     private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
@@ -42,6 +49,23 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
+        //Hookshot - Martin
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            RaycastHookshot(); // lance un raycast
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            cangrapple = false;// empêche de faire le grappin
+            line.enabled = false;
+        }
+        else if (cangrapple)
+        {
+            hookshot();// la traction du grappin
+            line.enabled = true;
+        }
+        
+
         #region Inputs & Animations
 
         bool jump = _jumpAction.ReadValue<float>() != 0;
@@ -69,13 +93,12 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// Applie a force to make the player jump et set the animation state.
+    /// Applie a force to make the player jump and set the animation state.
     /// </summary>
     private void Jump()
     {
         if (gc.isGrounded)
         {
-            Debug.Log("oui");
             anim.SetBool(IsGrounded, false);
             rb.AddForce(Vector2.up * defaultData.jumpForce, ForceMode2D.Impulse);
         }
@@ -121,5 +144,53 @@ public class PlayerController : MonoBehaviour
             #endregion
         }
 
+    }
+
+    private void RaycastHookshot() // raycast pour savoir si la distance du grappin au mur
+    {
+        RaycastHit2D raycast = Physics2D.Raycast(new Vector2(transform.position.x - 0.08f,transform.position.y -0.3f),
+            new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") ),100, groundlayer );
+
+        if(raycast.collider !=null)
+        {
+            linePosX = raycast.point.x;
+            linePosY = raycast.point.y;
+            cangrapple = true;
+            line.enabled = true;
+        }
+        else
+        {
+            cangrapple = false;
+        }
+        
+    }
+
+    void hookshot() // l'action de traction du grappin
+    {
+        rb.velocity = new Vector2(linePosX - transform.position.x, linePosY - transform.position.y).normalized *
+                      Mathf.Clamp(
+                          new Vector2(linePosX - transform.position.x, linePosY - transform.position.y).magnitude, 20,20);
+        ShowLign(linePosX, linePosY);
+        line.endWidth = 0.1f;
+        line.startWidth = 0.1f;
+        line.startColor = Color.magenta;
+        line.endColor = Color.yellow;
+    }
+
+    private void ShowLign(float posX, float posY) // montre la ligne du grappin
+    { 
+        if (cangrapple)
+        {
+            line.enabled = true;
+            line.positionCount = 2;
+            line.SetPosition(0,new Vector3(rb.position.x,rb.position.y, 0));
+            line.SetPosition(1,new Vector3(posX,posY,0));
+            line.endWidth = 0.5f;
+            line.startWidth = 0.5f;
+        }
+        else
+        {
+            line.enabled = false;
+        }
     }
 }
