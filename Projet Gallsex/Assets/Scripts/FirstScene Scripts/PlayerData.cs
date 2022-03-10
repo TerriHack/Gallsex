bf7b7cdb813b26801e389a2281a2bb502b29a622
Terri <A.dubuc@gmail.com>
2022-03-10 17:26:11 +0100

    Modif PlayerController

--- Projet Gallsex/Assets/Scripts/FirstScene Scripts/PlayerData.cs
+++ Projet Gallsex/Assets/Scripts/FirstScene Scripts/PlayerData.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Data", menuName = "Player Data", order = 1)]
+[CreateAssetMenu(fileName = "PlayerData", menuName = "Player Data", order = 1)]
 
 public class PlayerData : ScriptableObject
 {
-    [Header("Forces")] 
-    public AnimationCurve axisY;
-    public AnimationCurve axisX;
+    [Header("Forces")]
+    [Range(0f,100f)]
+    public float speed = 1f;
+    [Range(0f,100f)]
+    public float airSpeed = 0.9f;
+    [Range(0f,100f)]
+    public float jumpForce = 10f;
 }
--- Projet Gallsex/Assets/Scripts/TerriScene Scripts/PlayerControler.cs
+++ Projet Gallsex/Assets/Scripts/TerriScene Scripts/PlayerControler.cs
@@ -9,13 +9,7 @@ namespace TerriScene_Scripts
         private float _inputX;
         private bool _inputY;
         
-        
-        [Range(0f,100f)]
-        [SerializeField] private float speed = 1f;
-        [Range(0f,100f)]
-        [SerializeField] private float airSpeed = 0.9f;
-        [Range(0f,100f)]
-        [SerializeField] private float jumpForce = 60f;
+        [SerializeField] private PlayerData playerData;
         private float _maxSpeed = 15f;
         private float maxHeight = 35f;
         [SerializeField] private Rigidbody2D rb;
@@ -29,40 +23,31 @@ namespace TerriScene_Scripts
             _inputY = Input.GetKey(KeyCode.Space);
 
             #endregion
-            
-            Debug.Log(rb.velocity.x);
         }
 
         private void FixedUpdate()
         {
             if (_inputX != 0) HorizontalMove();
-            if (_inputY && isGrounded)
-            {
-                Jump();
-            }
+            if (_inputY && isGrounded) Jump();
             Clamping();
             Gravity();
         }
 
         private void HorizontalMove()
         {
-            Vector2 movement = new Vector2(_inputX * speed, 0);
+            Vector2 movement = new Vector2(_inputX * playerData.speed, 0);
 
             if (!isGrounded)
             {
-                movement = new Vector2(_inputX * airSpeed, 0);
+                movement = new Vector2(_inputX * playerData.airSpeed, 0);
             }
-            else
-            {
-                rb.gravityScale = 9f;
-            }
-            
+
             rb.AddForce(movement, ForceMode2D.Impulse);
         }
 
         private void Jump()
         {
-            Vector2 height = new Vector2(0, jumpForce);
+            Vector2 height = new Vector2(0, playerData.jumpForce);
             rb.AddForce(height,ForceMode2D.Impulse);
             isGrounded = false;
         }
@@ -74,23 +59,21 @@ namespace TerriScene_Scripts
             rb.velocity = new Vector2(horizontalVelocity, verticalVelocity);
         }
         
-        //jumptime - TIme.Time < 1 second
-
         private void OnCollisionEnter2D(Collision2D col)
         {
             isGrounded = col.GetContact(0).normal.y > 0.9f;
         }
-
-        private void OnCollisionStay2D(Collision2D col)
-        {
-            isGrounded = col.GetContact(0).normal.y > 0.9f;
-        }
-
+        
         private void Gravity()
         {
-            if (rb.velocity.y < - 0.5f)
+            if (rb.velocity.y < -0.5f && !isGrounded)
             {
-                rb.gravityScale += 4 + Time.fixedDeltaTime;
+                rb.gravityScale += 1f + Time.fixedDeltaTime;
+                isGrounded = false;
+            }
+            else
+            {
+                rb.gravityScale = 9f;
             }
         }
     }
