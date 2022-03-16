c78a5c8bfa8acc94dca43fb6b3eaeeb6ba14bfe0
TheHappyLenox <p.bayart@rubika-edu.com>
2022-03-16 17:00:18 +0100

    Bonnetwip

--- Projet Gallsex/Assets/Scripts/PaulinScene Scripts/HatAnchor.cs
+++ Projet Gallsex/Assets/Scripts/PaulinScene Scripts/HatAnchor.cs
@@ -6,19 +6,24 @@ public class HatAnchor : MonoBehaviour
 {
     public Vector2 partOffset = Vector2.zero;
     public float lerpSpeed = 10f;
-    private Transform[] hairParts;
-    private Transform hairAnchor;
-    private void Awake()
-    {
-        hairAnchor = GetComponent<Transform>();
-        hairParts = GetComponentsInChildren<Transform>();
-    }
+    [SerializeField] Transform[] hairParts;
+    //private Transform hairAnchor;
 
+    Vector2 targetPosition;
+    Vector2 newPositionLerped;
     private void Update()
     {
-        Transform piecetoFollow = hairAnchor;
+        //Transform piecetoFollow = hairAnchor;
+        for (int i = 1; i < hairParts.Length; i++)
+        {
+            targetPosition = (Vector2)hairParts[i-1].position + partOffset;
+            newPositionLerped = Vector2.Lerp(hairParts[i].position, targetPosition, Time.deltaTime * lerpSpeed);
 
-        foreach(Transform hairPart in hairParts)
+            hairParts[i].position = newPositionLerped;
+        }
+
+        /*
+        foreach (Transform hairPart in hairParts)
         {
             if (!hairPart.Equals(hairAnchor))
             {
@@ -29,5 +34,6 @@ public class HatAnchor : MonoBehaviour
                 piecetoFollow = hairPart;
             }
         }
+        */
     }
 }
