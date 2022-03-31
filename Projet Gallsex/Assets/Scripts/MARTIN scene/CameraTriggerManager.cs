16277ead92b0cd52ed492a26600fe36e39428e85
Mskutnik <m.skutnik@rubika-EDU.com>
2022-03-31 12:27:14 +0200

    DOTWEEN
    installé DOTween, fini les triggers pour la caméra

--- Projet Gallsex/Assets/Scripts/MARTIN scene/CameraTriggerManager.cs
+++ Projet Gallsex/Assets/Scripts/MARTIN scene/CameraTriggerManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraTriggerManager : MonoBehaviour
 {
@@ -9,12 +10,16 @@ public class CameraTriggerManager : MonoBehaviour
 
     public void EditScale(Vector2 newScale)
     {
-        Camera.GetComponent<Camera>().orthographicSize = 5;
+        //Camera.GetComponent<Camera>().orthographicSize = newScale.x;
+        DOTween.To(() => Camera.GetComponent<Camera>().orthographicSize,
+            x => Camera.GetComponent<Camera>().orthographicSize = x, newScale.x, 1);
     }
 
     public void EditOffset(Vector2 newOffset)
     {
         Camera.transform.GetComponent<camerafollow>().offset = new Vector3(newOffset.x,newOffset.y, -10);
+        DOTween.To(() => Camera.transform.GetComponent<camerafollow>().offset,
+            x => Camera.transform.GetComponent<camerafollow>().offset = x, new Vector3(newOffset.x,newOffset.y, -10), 1);
     }
 
     public void EditTracking(bool newTracking)
