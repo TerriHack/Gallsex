e2a70b0f84f0e803dd5f7bdadfc59b79769db281
TheHappyLenox <p.bayart@rubika-edu.com>
2022-04-02 13:27:14 +0200

    prefab light + update VFX

---
+++ Projet Gallsex/Assets/LightIntensity.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightIntensity : MonoBehaviour
+{
+    public Light2D lightFlamme;
+    private float graphValue;
+    public AnimationCurve curveIntensity;
+    public float playbackSpeed = 0;
+
+    void Update()
+    {
+        graphValue = curveIntensity.Evaluate(Time.time / playbackSpeed);
+        lightFlamme.intensity = graphValue;
+    }
+}
