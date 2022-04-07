1e8f4dc3d25451ea52f388ea9e8024838a3fc66c
TerriHack <A.dubuc@rubika-edu.com>
2022-04-07 13:58:51 +0200

    Clean up tha mess

--- Projet Gallsex/Assets/Scripts/TerriScene Scripts/Dash.cs
+++ Projet Gallsex/Assets/Scripts/TerriScene Scripts/Dash.cs
@@ -9,17 +9,17 @@ public class Dash : MonoBehaviour
     [SerializeField] private PlayerControllerData playerData;
     [SerializeField] private PlayerBetterController playerController;
 
-    public float _inputX;
-    public float _inputY;
-    public float canDash;
-    public float dashDelay;
-    public float dashCounter;
+    private float _inputX;
+    private float _inputY;
+    private float _canDash;
+    private float _dashDelay;
+    private float _dashCounter;
     
     public bool isDashing;
 
     private void Start()
     {
-        canDash = 0f;
+        _canDash = 0f;
     }
 
     void Update()
@@ -28,47 +28,49 @@ public class Dash : MonoBehaviour
         _inputX = Input.GetAxisRaw("Mouse X");
         _inputY = Input.GetAxisRaw("Mouse Y");
 
+        #region La vallé des IF
         if (_inputX > 0.5 || _inputX < -0.5 || _inputY > 0.5 || _inputY < -0.5)
         {
-            if (canDash == 0f)
+            if (_canDash == 0f)
             {
-                dashDelay = playerData.dashTime;
+                _dashDelay = playerData.dashTime;
             }
         }
 
-        if (playerController.isGrounded  && dashCounter <= 0f|| playerController.isTouchingFront && dashCounter <= 0f)
+        if (playerController.isGrounded  && _dashCounter <= 0f|| playerController.isTouchingFront && _dashCounter <= 0f)
         {
-            canDash = 0f;
+            _canDash = 0f;
         }
 
-        dashDelay -= Time.deltaTime;
-        
-        if (dashDelay >= 0f)
+        //This determines how long the force is added to the player dashing
+        if (_dashDelay >= 0f)
         {
             isDashing = true;
         }else
         {
             isDashing = false;
         }
-
-        dashCounter -= Time.deltaTime;
+        #endregion
+        
+        _dashDelay -= Time.deltaTime;
+        _dashCounter -= Time.deltaTime;
     }
 
     private void FixedUpdate()
     {
-        if (dashDelay >= 0f) Propulsion();
+        if (_dashDelay >= 0f) Propulsion();
     }
 
     private void Propulsion()
     {
         Vector2 direction = new Vector2(_inputX, _inputY);
         
-        if (canDash == 0f && !playerController.isTouchingFront && !playerController.isGrounded)
+        if (_canDash == 0f && !playerController.isTouchingFront && !playerController.isGrounded)
         {
-            dashCounter = playerData.dashCooldown;
+            _dashCounter = playerData.dashCooldown;
             rb.velocity = new Vector2(0, 0);
             rb.AddForce(direction.normalized * playerData.dashForce,ForceMode2D.Impulse); 
-            canDash += 1f;
+            _canDash += 1f;
         }
     }
 }
