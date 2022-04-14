fda33cd3cb47333e042dde0b68864c1505b7a360
TerriHack <A.dubuc@rubika-edu.com>
2022-04-14 20:38:40 +0200

    Oups voir le commit précédents ^^"

--- Projet Gallsex/Assets/Scripts/Boss/initiateBossFight.cs
+++ Projet Gallsex/Assets/Scripts/Boss/initiateBossFight.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public class initiateBossFight : MonoBehaviour
 {
-    public GameObject camera;
+    public GameObject cam;
     public GameObject cloud;
     public float cloudOffset;
     public void Horizontal(Vector3 startPos,Vector3 waypoint1, Vector3 waypoint2, float transitionTime)
     {
-        camera.GetComponent<camerafollow>().BossFight(startPos, waypoint1,waypoint2,transitionTime);
-        cloud.transform.SetParent(camera.transform);
+        cam.GetComponent<camerafollow>().BossFight(startPos, waypoint1,waypoint2,transitionTime);
+        cloud.transform.SetParent(cam.transform);
         //float orthographicSize = camera.GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize;
-        cloud.transform.position = new Vector3(camera.transform.position.x - cloudOffset, camera.transform.position.y);
+        cloud.transform.position = new Vector3(cam.transform.position.x - cloudOffset, cam.transform.position.y);
         if (cloud.transform.rotation != Quaternion.Euler(0, 0, 90))
         {
             cloud.transform.localRotation = Quaternion.Euler(0, 0, 90);
--- Projet Gallsex/Assets/Scripts/CameraRelated/CameraTriggerManager.cs
+++ Projet Gallsex/Assets/Scripts/CameraRelated/CameraTriggerManager.cs
@@ -6,19 +6,19 @@ using DG.Tweening;
 
 public class CameraTriggerManager : MonoBehaviour
 {
-    public GameObject camera;
+    public GameObject cam;
     public float tweenTime;
 
     public void EditScale(Vector2 newScale)
     {
-        DOTween.To(() => camera.GetComponent<Camera>().orthographicSize,
-            x => camera.GetComponent<Camera>().orthographicSize = x, newScale.x, tweenTime);
+        DOTween.To(() => cam.GetComponent<Camera>().orthographicSize,
+            x => cam.GetComponent<Camera>().orthographicSize = x, newScale.x, tweenTime);
     }
 
     public void EditOffset(Vector2 newOffset)
     {
-        DOTween.To(() => camera.transform.GetComponent<camerafollow>().offset,
-            x => camera.transform.GetComponent<camerafollow>().offset = x, new Vector3(newOffset.x,newOffset.y, -10), tweenTime);
+        DOTween.To(() => cam.transform.GetComponent<camerafollow>().offset,
+            x => cam.transform.GetComponent<camerafollow>().offset = x, new Vector3(newOffset.x,newOffset.y, -10), tweenTime);
     }
 
     /*public void EditTracking(bool newTracking)
