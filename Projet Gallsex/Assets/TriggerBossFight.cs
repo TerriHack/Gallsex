b8072610389ef6b9057fddf17cfaf375b170bf9e
Mskutnik <m.skutnik@rubika-EDU.com>
2022-05-04 18:47:49 +0200

    Découverte de la raison de bug de camerafollow :
    La cinémachine a été enlevée

--- Projet Gallsex/Assets/TriggerBossFight.cs
+++ Projet Gallsex/Assets/TriggerBossFight.cs
@@ -9,15 +9,14 @@ public class BossTriggers : MonoBehaviour
    public Vector3 waypoint1;
    public Vector3 waypoint2;
    public float speed;
-   public bool startsHorizontally;
-   public float cloudStartRotation;
    
    private void OnTriggerEnter2D(Collider2D other)
    {
       if (other.CompareTag("Player"))
       {
-         //other.GetComponent<initiateBossFight>().Horizontal(startPosition, waypoint1, waypoint2, speed,startsHorizontally,cloudStartRotation);
+         Debug.Log("entered");
          other.GetComponent<initiateBossFight>().Horizontal(startPosition, waypoint1,waypoint2,speed);
       }
    }
+
 }
