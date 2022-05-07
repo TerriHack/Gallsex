da8a95ad7bb49c0c5d317f240be622afcd7583f0
TerriHack <A.dubuc@rubika-edu.com>
2022-05-07 09:58:14 +0200

    Portage vfx playerbetter controller to vfx manager

--- Projet Gallsex/Assets/Scripts/PlayerRelated/VFXManager.cs
+++ Projet Gallsex/Assets/Scripts/PlayerRelated/VFXManager.cs
@@ -9,7 +9,10 @@ public class VFXManager : MonoBehaviour
     [SerializeField] private GameObject dash;   
     [SerializeField] private ParticleSystemRenderer dashVFX;
     [SerializeField] private GameObject landing;
-    [SerializeField] private GameObject wallJump; 
+    [SerializeField] private GameObject wallJump;
+    [SerializeField] private GameObject wallSlide;
+    [SerializeField] private GameObject run;
+    [SerializeField] private GameObject jump;
 
     private Vector2 _feetPos;
     private Vector2 _wallContact;
@@ -17,15 +20,20 @@ public class VFXManager : MonoBehaviour
     public bool isDashingRight;
     public bool isDashingLeft;
     public bool isLanding;
-    public bool isWallJumping;
     public bool isWallJumpingRight;
     public bool isWallJumpingLeft;
+    public bool isWallSliding;
+    public bool isRunning;
+    public bool isJumping;
 
     private void Update()
     {
         if(isLanding) LandingVFX();
         if (isDashingRight || isDashingLeft) DashVFX();
         if(isWallJumpingLeft || isWallJumpingRight) Walljump();
+        WallSliding();
+        Running();
+        if(isJumping) Jumping();
     }
     
     private void LandingVFX()
@@ -34,7 +42,6 @@ public class VFXManager : MonoBehaviour
         Instantiate(landing, _feetPos, playerTr.rotation);
         isLanding = false;
     }
-
     private void DashVFX()
     {
         if(isDashingLeft)
@@ -50,7 +57,6 @@ public class VFXManager : MonoBehaviour
         isDashingRight = false;    
         isDashingLeft = false;
     }
-
     private void Walljump()
     {
         _wallContact = new Vector2(playerTr.position.x + 0.5f, playerTr.position.y - 0.7f);
@@ -72,6 +78,20 @@ public class VFXManager : MonoBehaviour
             isWallJumpingRight = false;
 
         }
-
+    }
+    private void WallSliding()
+    {
+        if(isWallSliding) wallSlide.SetActive(true);
+        else wallSlide.SetActive(false);
+    }
+    private void Running()
+    { 
+        if(isRunning) run.SetActive(true);
+        else run.SetActive(false);
+    }
+    private void Jumping()
+    {
+        Instantiate(jump, _feetPos, playerTr.rotation);
+        isJumping = false;
     }
 }
