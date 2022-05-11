cdfb638cd214aa7ad91059b8fa97d654bded17eb
Thomas Mottet <t.mottet@rubika-edu.com>
2022-05-11 12:37:36 +0200

    Corrections SoundDesign Prefab Player

--- Projet Gallsex/Assets/Scripts/AudioManager.cs
+++ Projet Gallsex/Assets/Scripts/AudioManager.cs
@@ -1,17 +1,15 @@
-using System;
 using System.Collections.Generic;
-using System.Security;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    #region Variables
+    #region AudioSources
 
     [SerializeField] private AudioSource playerAudioSource;
 
-    [Header("AudioClips")] [SerializeField]
-    private AudioClip run;
-
+    [Header("AudioClips")] 
+    
+    [SerializeField] private AudioClip run;
     [SerializeField] private AudioClip jump;
     [SerializeField] private AudioClip landing;
     [SerializeField] private AudioClip wallSlide;
@@ -19,7 +17,30 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioClip crouch;
     [SerializeField] private AudioClip dash;
 
-    public List<AudioClip> soundList = new List<AudioClip>();
+    private List<AudioClip> soundList = new List<AudioClip>();
+    
+    #endregion
+    
+    #region Volumes
+
+    [Header("Volumes")] 
+    
+    [Range(0f, 1f)]
+    [SerializeField] private float runVolume;
+    [Range(0f, 1f)]
+    [SerializeField] private float jumpVolume;
+    [Range(0f, 1f)]
+    [SerializeField] private float landingVolume;
+    [Range(0f, 1f)]
+    [SerializeField] private float wallSlideVolume;
+    [Range(0f, 1f)]
+    [SerializeField] private float sitVolume;
+    [Range(0f, 1f)]
+    [SerializeField] private float crouchVolume;
+    [Range(0f, 1f)]
+    [SerializeField] private float dashVolume;
+    
+    private List<float> volumeList = new List<float>();
 
     #endregion
 
@@ -28,11 +49,12 @@ public class AudioManager : MonoBehaviour
     public void Awake()
     {
         soundList.AddRange(new AudioClip[] {run, jump, landing, wallSlide, sit, crouch, dash});
+        volumeList.AddRange(new float[] {runVolume, jumpVolume, landingVolume, wallSlideVolume, sitVolume, crouchVolume, dashVolume});
     }
 
     public void StartSound(int _clip)
     {
-        playerAudioSource.PlayOneShot(soundList[_clip]);
+        playerAudioSource.PlayOneShot(soundList[_clip], volumeList[_clip]);
     }
 
     #endregion
