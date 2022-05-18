cdb2ac6d5ba5946111c4155c61a9f5110d92c0c4
Mskutnik <m.skutnik@rubika-EDU.com>
2022-05-18 14:47:12 +0200

    changement de scene pour update LD
    création d'un nouveau script pour reprendre à 0

--- Projet Gallsex/Assets/BossKIllTrigger.cs
+++ Projet Gallsex/Assets/BossKIllTrigger.cs
@@ -11,19 +11,14 @@ public class BossKIllTrigger : MonoBehaviour
     public float maxSpeed;
     public float minSpeed;
 
-    private Vector3 respawnPosition;
     [SerializeField] List<Vector3> waypoints;
     private int currentWaypointIndex;
     public float toNextWaypoint;
 
-    [SerializeField] private bool hasTweened;
     public float tweenTime;
-    private bool horizontal;
 
     private Vector2 lastPosition;
-    public GameObject particles;
-    
-    private Vector3 mouthRotation;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -65,7 +60,7 @@ public class BossKIllTrigger : MonoBehaviour
         GameObject playerWaypoint = GameObject.FindGameObjectWithTag("BossWaypoint");
         waypoints[0] = playerWaypoint.transform.position;
         
-        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex]) > toNextWaypoint)
+        if (Vector3.Distance(transform.position, player.transform.position) > toNextWaypoint)
         {
             speed *= speedFactor;
             if (speed > maxSpeed)
@@ -74,87 +69,36 @@ public class BossKIllTrigger : MonoBehaviour
             }
 
         }
-        else if (currentWaypointIndex + 1 < waypoints.Count)
-        {
-            if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex]) < toNextWaypoint && hasTweened == false)
-            {
-                TweenRotate();
-            }
-        }
 
 
-        if (Vector2.Distance(waypoints[currentWaypointIndex], transform.position) < .5f)
+        if (Vector2.Distance(player.transform.position, transform.position) < .5f)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -10);
             currentWaypointIndex++;
-            hasTweened = false;
             if (currentWaypointIndex >= waypoints.Count)
             {
                 GetComponent<BossKIllTrigger>().enabled = false;
                 currentWaypointIndex = 0;
             }
-            else
-            {
-                respawnPosition = waypoints[currentWaypointIndex];
-            }
         }
         
         transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex], Time.deltaTime * speed);
-        mouthRotation = playerWaypoint.transform.position - transform.position;
         //transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, Vector2.Angle(playerWaypoint.transform.position,transform.position));
         /*transform.LookAt(mouthRotation);
         transform.rotation = new quaternion(0, 0, transform.rotation.z *-1, transform.rotation.w);*/
         //Debug.Log(Vector2.Angle(playerWaypoint.transform.position, transform.position));
         
-        Vector2 angle = playerWaypoint.transform.position - transform.position;
+        /*Vector2 angle = playerWaypoint.transform.position - transform.position;
         transform.eulerAngles = new Vector3( transform.rotation.x, transform.rotation.y, Vector2.Angle(playerWaypoint.transform.position-transform.position,Vector2.right));
-        Debug.Log(angle);
+        Debug.Log(angle);*/
+        transform.LookAt(playerWaypoint.transform.position);
     }
 
     public void Activate(Vector3 startPos, List<Vector3> waypointList, bool isHorizontal)
     {
         GetComponent<BossKIllTrigger>().enabled = true;
-        horizontal = isHorizontal;
-        respawnPosition = startPos;
         transform.position = startPos;
         waypoints = waypointList;
     }
 
-    void TweenRotate()
-    {
-        if (horizontal)
-        {
-            if (waypoints[currentWaypointIndex].y > waypoints[currentWaypointIndex + 1].y)
-            {
-                // going down
-            }
-            else
-            {
-                //going up
-                DOTween.To(() => transform.rotation, x => transform.rotation = x, new Vector3(transform.rotation.x, transform.rotation.y, 90), tweenTime);
-            }
-
-            horizontal = false;
-        }
-        else
-        {
-            if (waypoints[currentWaypointIndex].x < waypoints[currentWaypointIndex + 1].x)
-            {
-                //going right
-                DOTween.To(() => transform.rotation, x => transform.rotation = x,
-                    new Vector3(transform.rotation.x, transform.rotation.y, 0), tweenTime);
-            }
-            else
-            {
-                //going left
-                DOTween.To(() => transform.rotation, x => transform.rotation = x,
-                    new Vector3(transform.rotation.x, transform.rotation.y, 180), tweenTime);
-            }
-            Debug.Log("tweenHorizontal");
-
-            horizontal = true;
-        }
-
-        hasTweened = true;
-    }
 }
---
+++ Projet Gallsex/Assets/BossMovement.cs
@@ -0,0 +1,162 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    public float maxSpeed;
+    public float newMaxSpeed;
+    public float changeMaxSpeed;
+    public float minSpeed;
+    public float newMinSpeed;
+    public float speedFactor;
+    [SerializeField] private float speed;
+
+    public GameObject player;
+
+    [SerializeField] private List<Vector3> waypoints;
+    private int currentWaypointIndex;
+    public float toNextWaypoint;
+
+    private float yPositionDifference;
+    public float speedUpDistance;
+    public float slowDownDistance;
+
+    public bool isHorizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (maxSpeed == 0)
+        {
+            maxSpeed = 12;
+            newMaxSpeed = maxSpeed;
+        }
+
+        if (minSpeed == 0)
+        {
+            minSpeed = 1;
+            newMinSpeed = minSpeed;
+        }
+
+        if (speedFactor == 0)
+        {
+            speedFactor = 1.018f;
+        }
+
+        if (speed == 0)
+        {
+            speed = 0.5f;
+        }
+
+        if (changeMaxSpeed == 0)
+        {
+            changeMaxSpeed = 30;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeSpeed();
+        NormalMovement();
+        MoveMouth();
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex], Time.deltaTime * speed);
+        
+    }
+
+
+    private void NormalMovement()
+    {
+        
+        if (Vector2.Distance(waypoints[currentWaypointIndex], transform.position) < .5f) // if at a waypoint
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Count)
+            {
+                GetComponent<BossMovement>().enabled = false;
+                currentWaypointIndex = 0;
+            }
+        }
+
+    }
+
+    private void MoveMouth()
+    {
+        if (isHorizontal)
+        {
+            if (Vector2.Distance(new Vector2(0, transform.position.y), new Vector2(0, player.transform.position.y)) < yPositionDifference)
+            {
+                Vector3.MoveTowards(transform.position,
+                    new Vector3(transform.position.x, transform.position.y + 1, transform.position.z),
+                    Time.deltaTime * speed);
+            }
+            else if (Vector2.Distance(new Vector2(0, transform.position.y), new Vector2(0, player.transform.position.y)) > -yPositionDifference)
+            {
+            
+                Vector3.MoveTowards(transform.position,
+                    new Vector3(transform.position.x, transform.position.y - 1, transform.position.z),
+                    Time.deltaTime * speed);
+            }
+        }
+        else
+        {
+            if (Vector2.Distance(new Vector2(transform.position.x,0), new Vector2(player.transform.position.x, 0)) < yPositionDifference)
+            {
+                Vector3.MoveTowards(transform.position,
+                    new Vector3(transform.position.x + 1, transform.position.y, transform.position.z),
+                    Time.deltaTime * speed);
+            }
+            else if (Vector2.Distance(new Vector2(transform.position.x, 0), new Vector2(player.transform.position.x, 0)) > -yPositionDifference)
+            {
+            
+                Vector3.MoveTowards(transform.position,
+                    new Vector3(transform.position.x - 1, transform.position.y, transform.position.z),
+                    Time.deltaTime * speed);
+            }
+        }
+    }
+
+    private void ChangeSpeed()
+    {
+        if (speed < newMaxSpeed)
+        {
+            speed *= speedFactor;
+            if (speed > newMaxSpeed)
+            {
+                speed = newMaxSpeed;
+            }
+        }
+
+        if (speed < minSpeed)
+        {
+            speed = minSpeed;
+        }
+        
+        
+        
+        if (Vector2.Distance(transform.position, player.transform.position) > speedUpDistance)
+        {
+            newMaxSpeed = changeMaxSpeed;
+            Debug.Log("speeding up");
+        }
+
+        if (Vector2.Distance(transform.position, player.transform.position) < slowDownDistance)
+        {
+            newMaxSpeed /= speedFactor * 2;
+            Debug.Log("slowing");
+        }
+    }
+    
+    public void Activate(Vector3 startPos, List<Vector3> list)
+    {
+        Start();
+        GetComponent<BossMovement>().enabled = true;
+        transform.position = startPos;
+        for (int i = 0; i < list.Count; i++)
+        {
+            waypoints.Add(list[i]);
+        }
+    }
+}
