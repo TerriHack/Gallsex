400bb46117b25a1b2a75728843a683f129458e83
TerriHack <A.dubuc@rubika-edu.com>
2022-05-26 16:27:04 +0200

    Fix UI in game

---
+++ Projet Gallsex/Assets/Scripts/PlayerRelated/DataSave.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+// public class DataSave
+// {
+//     public float goldTime;
+//     public float silverTime;
+//     public float bronzeTime;
+//
+//     public DataSave(GameManager gameManager)
+//     {
+//         public float goldTime;
+//         public float silverTime;
+//         public float bronzeTime;
+//     }
+// }
+
