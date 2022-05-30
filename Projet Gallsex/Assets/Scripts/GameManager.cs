bbd09aa438b7eca99fb1d535d3fefdfb21c2d690
TerriHack <A.dubuc@rubika-edu.com>
2022-05-30 13:34:24 +0200

    Prep build

--- Projet Gallsex/Assets/Scripts/GameManager.cs
+++ Projet Gallsex/Assets/Scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+        Cursor.visible = false;
         DontDestroyOnLoad(this.gameObject);
         quality = 2;
         timerActive = false;
