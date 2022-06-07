8bfafeab266f77190f21eba227e578ea7bbe6779
TerriHack <a.dubuc@rubika-edu.com>
2022-06-08 01:18:46 +0200

    Sound Transition lvl 2 to Boss Scene

--- Projet Gallsex/Assets/SoundTransition.cs
+++ Projet Gallsex/Assets/SoundTransition.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,24 @@ using UnityEngine;
 public class SoundTransition : MonoBehaviour
 {
     public bool inZone;
-    
-    void 
+    private AudioSource music;
+    private Transform playerTr;
+
+    [SerializeField] private AudioSource nightmare;
+
+
+    private void Start()
+    {
+        music = GameObject.FindWithTag("GameManager").GetComponent<AudioSource>();
+        playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            nightmare.volume = 0.1f * Vector2.Distance(playerTr.position, transform.position);
+            music.volume = -0.1f * Vector2.Distance(playerTr.position, transform.position);
+        }
+    }
 }
